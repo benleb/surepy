6a0dc6487ec8ce2823cfb39d5ef6226962df2dc9
Ben Lebherz <git@benleb.de>
2020-04-30 13:01:30 +0200

    Bump version: 0.2.4 → 0.2.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='surepy',
-      # version='0.2.4',
+      # version='0.2.5',
       license='MIT',
       url='http://github.com/benleb/surepy',
       author='Ben Lebherz',

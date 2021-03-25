a53dad477b8c4f28794dad9015454100421ebb4a
Ben Lebherz <git@benleb.de>
2021-03-25 01:24:59 +0100

    group devices

---
+++ surepy/devices/__init__.py
@@ -0,0 +1,3 @@
+from surepy.devices.flap import Flap
+# from surepy.devices.feeder import Feeder
+from surepy.devices.hub import Hub
---
+++ surepy/devices/hub.py
@@ -0,0 +1,14 @@
+"""
+surepy.devices.hub
+====================================
+Class representing a Sure Petcare Hub.
+
+|license-info|
+"""
+
+
+from surepy.entities import SurepyDevice
+
+
+class Hub(SurepyDevice):
+    """Sure Petcare Hub."""

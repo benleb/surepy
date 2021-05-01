f5498b6c7e6c90e9ee90ec7349b10d43be0ec225
Laura Gutierrez Funderburk <laura_gutierrez_funderburk@sfu.ca>
2021-05-01 11:49:37 -0700

    adding docstring Pet class
    Description of various attributes in pet class

--- surepy/entities/pet.py
+++ surepy/entities/pet.py
@@ -24,6 +24,17 @@ from surepy.enums import EntityType, FoodType, Location
 
 
 class Pet(SurepyEntity):
+    """
+    Represents pet. Contains attributes of the pet.
+
+    Attributes obtained through Sure PetCare API and include
+     - pet ID (int) provided by Sure PetCare
+     - pet type default always "pet"
+     - pet data: raw output from Sure PetCare API
+     - pet name (string) name of pet
+     - pet state (integer in API, string in script), one of "outside" or "home"
+
+    """
     def __init__(self, data: dict[str, Any]):
 
         super().__init__(data=data)
